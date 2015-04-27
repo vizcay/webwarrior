f1192ec978fbacb784be8fc3fa489a3878ee7919
Pablo Vizcay <pablo.vizcay@gmail.com>
2015-04-27 17:41:06 -0300

    added mising tag.rb file

---
+++ app/models/tag.rb
@@ -0,0 +1,15 @@
+class Tag
+  attr_accessor :name, :tasks
+
+  def initialize
+    @tasks = []
+  end
+
+  def pending_tasks
+    tasks.select { |t| t.status == :pending }.sort_by { |t| -t.urgency }
+  end
+
+  def active?
+    not pending_tasks.empty?
+  end
+end
